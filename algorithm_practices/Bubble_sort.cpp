55da303790b5bb203992bb90af05dbe11a03b5f7
Blake <56631911+BlakeLin-LSY@users.noreply.github.com>
2022-08-05 15:56:22 +0800

    Update bubble sort

--- algorithm_practices/Bubble_sort.cpp
+++ algorithm_practices/Bubble_sort.cpp
@@ -1,29 +1,27 @@
 //  Bubble sort
-//      
-//      
+//      Brute force sorting algorithm
+//      Compete with "Insertion sort", "Selection sort"
 //
 //  Complexity:
 //
-//  Time:       O()
-//  Average:    O()
-//  Best:       O()
-//  Worst:      O()
+//  Time:       O(n^2)
+//  Average:    O(n^2)
+//  Best:       O(n^2) -> O(n) if adding a flag variable
+//  Worst:      O(n^2)
 //
-//  Space:      O()
-//  Stability:  
+//  Space:      O(1)
+//  Stability:  Yes
 
 //  Pros:    
-//      -   
-//      -   
+//      -   Easy and straightforward
+//      -   In-place algorithm (no extra space needed)
 //      -   
 //  Cons:    
-//      -   
-//      -   
-//      -   
+//      -   Worst efficiency as "too many swaps" are required in comparison to selection and insertion
 //      -   
 
 // Reference
-// 
+// https://www.mygreatlearning.com/blog/bubble-sort/
 // 
 
 
@@ -31,8 +29,45 @@
 
 using namespace std;
 
+// For ascending order
+void bubblesort(int arr[], int n){
+    for(int i=0; i<n-1; ++i){
+        for(int j=0; j<n-i-1; ++j){
+            if(arr[j] > arr[j+1]) swap(arr[j], arr[j+1]);
+        }
+    }
+}
+
+// modified bubble-sort T(O) == O(n) in best case;
+void bubblesort_m(int arr[], int n){
+    int flag;
+    for(int i=0; i<n-1; ++i){
+        flag=0;
+        for(int j=0; j<n-i-1; ++j){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                flag=1;
+            }
+        }
+        if(flag=0) break;
+    }
+}
+
+void display(int arr[], int n){
+    for(int i=0; i<n; ++i){
+        cout << arr[i] << " ";
+    }cout << endl;
+}
 
 int main(){
-
+    int arr[] = {-2, 45, 0, 11, -9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    
+    cout << "Original array :\n";
+    display(arr, size);
+    bubblesort(arr, size);
+    cout << "Sorted array :\n";
+    display(arr, size);
     return 0;
 }
