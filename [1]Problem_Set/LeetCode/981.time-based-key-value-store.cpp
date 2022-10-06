f926c08c91027555caa5e40dc9680264dbd74fb1
Blake <56631911+BlakeLin-LSY@users.noreply.github.com>
2022-10-06 22:39:11 +0800

    Add daily challenge 981. key-timestamp table

---
+++ [1]Problem_Set/LeetCode/981.time-based-key-value-store.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=981 lang=cpp
+ *
+ * [981] Time Based Key-Value Store
+ */
+
+// @lc code=start
+class TimeMap {
+public:
+    unordered_map<string, map<int, string>> KTmap;
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        KTmap[key][timestamp]=value;
+    }
+    
+    string get(string key, int timestamp) {
+        auto it=KTmap[key].upper_bound(timestamp);
+        if(it == KTmap[key].begin()) return "";
+        it--;
+        return it->second;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
+// @lc code=end
+
