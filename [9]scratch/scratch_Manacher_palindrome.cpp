87313e83927287f77a2c83073ccc9944fefbb17e
Blake <56631911+BlakeLin-LSY@users.noreply.github.com>
2022-11-17 17:31:37 +0800

    Add Manacher algorithm scratch

---
+++ [9]scratch/scratch_Manacher_palindrome.cpp
@@ -0,0 +1,15 @@
+/*
+    Manacher's algorithm 
+    O(N) solution for finding longest palindrome.
+
+    Reference:
+    https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    
+}
